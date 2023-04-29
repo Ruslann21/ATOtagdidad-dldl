98b6f0b659c7b2924e4ec4645e99ccfee6ab8d51
KenanKodes <75477114+KenanKodes@users.noreply.github.com>
2023-04-29 21:34:27 +0400

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID", "5539983698"))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/9656bed2d9359c9675f52.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/9656bed2d9359c9675f52.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/3ac497b59a221257ef441.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/3ac497b59a221257ef441.jpg")
 
 SESSION = getenv("SESSION", "AgBsONl6wdGgTo_hj7wZXLZJZlMn9gXOjkePS7k8vow-8yJK2SAK7rHTmwXT91K9oIz66u_L0NkLkt7HqR3zImVXUtiCQsGeg_HGIkQrS0Q0A2B3GAyy6DKEqBXZz4tK7KV645TBv6ZfHnnFd1mCihHMW3cxaUVw9rv4DDFmnlFGU--gnOJtKG-QKdj9N9T4OxLJVKo46gewb1Ftx5iOQWX5BrqTA6osVDTuanfyKFF5hRE_V0IT2SGWpIYRUgdIB4jjx3pXKWytsJSLuRlgQNVzPwdh72cQ8Bnv3410Novd6rhBQAdlsusSX_NZyoTFeic9xfOLfc_RuquBKe-8DhuiAAAAAVzTqCsA")
 
